8b28320c899db0ede1c4ad07976e485556c339ab
NecatiY <41998208+NecatiY@users.noreply.github.com>
2018-08-01 16:40:10 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+class main{
+  public static void main(String[] args) {
+    String myVar = "Hello ";
+    //byte b = 128;
+    float f = 1.4f;
+
+    int i = 1234;
+    //int i3 = d + 5;
+    
+
+    String name;
+    name = "Necati";
+    int i4 = 0;
+    //double d2;
+    double d2 = 0;
+
+    boolean b = false;
+    b = true;
+
+//Hello Necati!
+    System.out.println(myVar + name + "!");
+  }
+}
